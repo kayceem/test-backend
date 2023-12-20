d03ff0bf2cfd63e378ce32b7c40a0bfa92633a24
Quoc Trung <trungqt1905@gmail.com>
2023-12-20 13:27:15 +0700

    update blog Time

--- models/Blog.js
+++ models/Blog.js
@@ -2,52 +2,55 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const blogSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  author: {
-    type: String,
-    required: true,
-  },
-  blogImg: {
-    type: String,
-    required: true,
-  },
-  technology: {
-    type: String,
-    required: true,
-  },
-  tags: [
-    {
-      type: String,
-    },
-  ],
-  readTime: {
-    type: String,
-    required: true,
-  },
-  datePublished: {
-    type: Date,
-    default: Date.now,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  userId: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  category: [
-    {
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
       type: String,
       required: true,
     },
-  ],
-});
+    author: {
+      type: String,
+      required: true,
+    },
+    blogImg: {
+      type: String,
+      required: true,
+    },
+    technology: {
+      type: String,
+      required: true,
+    },
+    tags: [
+      {
+        type: String,
+      },
+    ],
+    readTime: {
+      type: String,
+      required: true,
+    },
+    datePublished: {
+      type: Date,
+      default: Date.now,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    category: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 blogSchema.index({ title: "text", content: "text" });
 module.exports = mongoose.model("Blog", blogSchema);
