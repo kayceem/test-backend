beeb6da912d1e28ca0281d0ebbb4f59c96fcc9eb
Quoc Trung <trungqt1905@gmail.com>
2024-01-09 16:01:30 +0700

    fig bug and update reply comments
    fig bug and update reply comments

--- routes/comments.ts
+++ routes/comments.ts
@@ -5,24 +5,28 @@ import {
   getComments,
   updateComment,
   deleteComment,
-  likeComment
-} from '../controllers/comments'; 
+  likeComment,
+  replyComment,
+} from "../controllers/comments";
 
 const router = express.Router();
 
 // Route để tạo một bình luận mới
-router.post('/', createComment);
+router.post("/", createComment);
 
 // Route để lấy tất cả bình luận của một bài viết
-router.get('/:postId', getComments);
+router.get("/:postId", getComments);
 
 // Route để cập nhật một bình luận
-router.put('/:commentId', updateComment);
+router.put("/:commentId", updateComment);
 
 // Route để xóa một bình luận
-router.delete('/:commentId', deleteComment);
+router.delete("/:commentId", deleteComment);
 
 // Route để 'like' hoặc 'unlike' một bình luận
-router.patch('/like/:commentId', likeComment);
+router.patch("/like/:commentId", likeComment);
+
+// Route để phản hồi bình luận
+router.post("/reply/:commentId", replyComment);
 
 export default router;
