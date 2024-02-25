73df5586343fb6c73760cdc3e0806221f7c646b5
sangtrandev00 <nhatsang0101@gmail.com>
2024-02-25 16:57:34 +0700

    up code for test schema

--- routes/admin/test.ts
+++ routes/admin/test.ts
@@ -1,13 +1,26 @@
 import { Router } from "express";
 import uploadMiddleware from "../../middleware/upload";
 import * as testController from "../../controllers/admin/test";
+import isAuth from "../../middleware/is-auth";
 
 const router = Router();
 
-router.get("/", testController.getTests);
+/** load test */
+router.get("/",isAuth, testController.getTests);
 
-router.get("/test/:testId", testController.getTestById);
+/** load detail single test */
+router.get("/test/:testId", isAuth,testController.getTestById);
 
-router.post("/test/create", uploadMiddleware.array("images[]"), testController.postTest);
+/** get histories of test */
+router.get("/test/:testId/histories", isAuth,testController.loadHistories);
+
+/** create data for test */
+router.post("/test/create",isAuth, uploadMiddleware.array("images[]"), testController.postTest);
+
+/** Update a test */
+router.put("/test/update",isAuth, uploadMiddleware.array("images[]"), testController.updateTest);
+
+/** Update active status test */
+router.patch("/test/update_active_status", isAuth,uploadMiddleware.array("images[]"), testController.updateActiveStatusTest);
 
 export default router;
