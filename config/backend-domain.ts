a634af18ce1a2b667e11c06ef3a4b7c3766c2350
Quoc Trung <trungqt1905@gmail.com>
2024-03-04 13:06:44 +0700

    ELP-246-them-chức-nang-phục-hồi-mật-khẩu-ở-trang-quản-trị-admin-api
     ELP-246-them-chức-nang-phục-hồi-mật-khẩu-ở-trang-quản-trị-admin-api

--- config/backend-domain.ts
+++ config/backend-domain.ts
@@ -1,6 +1,5 @@
 // const BACKEND_URL = `https://e-learning-api-dev.trannhatsang.com`; // dev site
-// const BACKEND_URL = `https://e-learning-api.trannhatsang.com`; // production site
-
+// const BACKEND_URL = `https://e-learning-api.trannhatsang.com`; // production site/
 const BACKEND_URL = "http://localhost:9000"; // Local site!!!
 
 export { BACKEND_URL };
