e8d547126b6917fa0f1669ce82f6b7507d164bf9
Quoc Trung <trungqt1905@gmail.com>
2024-03-15 04:32:53 +0700

    ELP-313-bug-chưa-co-chức-nang-thảo-luận-api
    ELP-313-bug-chưa-co-chức-nang-thảo-luận-api

---
+++ types/IDiscussCourse.type.ts
@@ -0,0 +1,14 @@
+import { Document, Schema } from "mongoose";
+import { IBaseSchema } from "./base.type";
+
+export interface IdiscussCourse extends IBaseSchema {
+  title: string;
+  code: string;
+  comments: string;
+  userId: Schema.Types.ObjectId;
+  discussId: Schema.Types.ObjectId;
+  parentDiscussId?: Schema.Types.ObjectId;
+  replies: Schema.Types.ObjectId[];
+  lessonId: Schema.Types.ObjectId;
+  courseId: Schema.Types.ObjectId;  
+}
